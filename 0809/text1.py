ca5e804649187741ce02036a3f4ada054e076831
neomichael <141106632+neomichael@users.noreply.github.com>
2026-08-09 22:09:06 +0800

    remove api key 2026-08-09-2208
    feel strane because local file has been remove by server site did not. so manual remove

--- 0809/text1.py
+++ 0809/text1.py
@@ -1,6 +1,5 @@
 from google import genai
 
-# client = genai.Client(api_key="AQ.Ab8RN6IYgL4wQF0dPwb_MC7P7GxOnurjx-d1e0l2LahZ2fPA_w")
 client = genai.Client()
 
 interaction = client.interactions.create(
